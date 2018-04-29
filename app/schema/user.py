31cf7424cccde5cf0748be66f4e46f8257d29921
marvinkome <marvinkome@gmail.com>
2018-04-30 00:28:19 +0100

    Token required to make mutation

--- app/schema/user.py
+++ app/schema/user.py
@@ -1,6 +1,9 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType
+from graphql import GraphQLError
+from flask_jwt_extended import get_jwt_identity
+
 from ..model import User as UserModel
 from .. import db
 
@@ -11,13 +14,16 @@ class User(SQLAlchemyObjectType):
 
 class UpdateProfilePic(graphene.Mutation):
     class Arguments:
-        user_id = graphene.Int(required=True)
         new_pic = graphene.String(required=True)
 
     user = graphene.Field(lambda: User)
 
-    def mutate(self, info, new_pic, user_id):
-        user = UserModel.query.filter_by(uuid=user_id).first()
+    def mutate(self, info, new_pic):
+        email = get_jwt_identity()
+        if email is None:
+            return GraphQLError('You need an access token to perform this action')
+
+        user = UserModel.query.filter_by(email=email).first()
         if user is not None:
             user.gravatar_url = new_pic
             
@@ -27,14 +33,18 @@ class UpdateProfilePic(graphene.Mutation):
 
 class UpdateInfo(graphene.Mutation):
     class Arguments:
-        user_id = graphene.Int(required=True)
         new_full_name = graphene.String()
         new_description = graphene.String()
 
     user = graphene.Field(lambda: User)
 
-    def mutate(self, info, user_id, new_full_name=None, new_description=None):
-        user = UserModel.query.filter_by(uuid=user_id).first()
+    def mutate(self, info, new_full_name=None, new_description=None):
+        email = get_jwt_identity()
+        if email is None:
+            return GraphQLError('You need an access token to perform this action')
+            
+        user = UserModel.query.filter_by(email=email).first()
+        
         if user is not None:
             if new_full_name is not None:
                 user.full_name = new_full_name
