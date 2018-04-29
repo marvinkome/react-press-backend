70d277edce7fc5a6763f4936d0afb689a8cabf67
marvinkome <marvinkome@gmail.com>
2018-04-29 04:11:20 +0100

    Corrected config and prepared for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,9 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+        from flask_sslify import SSLify
+        SSLify(app)
 
     db.init_app(app)
     jwt.init_app(app)
--- config.py
+++ config.py
@@ -26,6 +26,8 @@ class Config:
     ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
     FLASKY_DB_QUERY_TIMEOUT = 0.5
 
+    SSL_DISABLE = True
+
     @staticmethod
     def init_app(app):
         pass
@@ -40,32 +42,28 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', postgres_local_base + database_name)
+    SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    JWT_SECRET_KEY = os.environ.get('SECRET_KEY')
     
     @classmethod
     def init_app(cls, app):
         Config.init_app(app)
-    
-        # # email errors to the administrators
-        # import logging
-        # from logging.handlers import SMTPHandler
-        
-        # credentials = None
-        # secure = None
-        # if getattr(cls, 'MAIL_USERNAME', None) is not None:
-        #     credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
-        #     if getattr(cls, 'MAIL_USE_TLS', None):
-        #         secure = ()
-            
-        #     mail_handler = SMTPHandler(
-        #                         mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
-        #                         fromaddr=cls.FLASKY_MAIL_SENDER,
-        #                         toaddrs=[cls.FLASKY_ADMIN],
-        #                         subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
-        #                         credentials=credentials,
-        #                         secure=secure)
 
-        #     mail_handler.setLevel(logging.ERROR)
-        #     app.logger.addHandler(mail_handler)
+class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+        
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(file_handler)
+
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
 
 config = {
     'development': DevelopmentConfig,
