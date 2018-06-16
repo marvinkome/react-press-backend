80bab2c05354ea1d124791c52b25330f3d5a3e67
marvinkome <marvinkome@gmail.com>
2018-06-16 01:13:03 +0100

    change runserver class

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os
 from app import create_app, db, socket, migrate
 from app.model import User, Post, Comment, CommentReply, Clap, Tags, Notification
-from flask_script import Manager, Shell, Server
+from flask_script import Manager, Shell, Server as _Server, Option
 from flask_migrate import Migrate, MigrateCommand
 
 
@@ -51,8 +51,63 @@ def make_shell_context():
         Notification=Notification
     )
 
+class Server(_Server):
+    help = description = 'Runs the Socket.IO web server'
+
+    def get_options(self):
+        options = (
+            Option('-h', '--host',
+                   dest='host',
+                   default=self.host),
+
+            Option('-p', '--port',
+                   dest='port',
+                   type=int,
+                   default=self.port),
+
+            Option('-d', '--debug',
+                   action='store_true',
+                   dest='use_debugger',
+                   help=('enable the Werkzeug debugger (DO NOT use in '
+                         'production code)'),
+                   default=self.use_debugger),
+            Option('-D', '--no-debug',
+                   action='store_false',
+                   dest='use_debugger',
+                   help='disable the Werkzeug debugger',
+                   default=self.use_debugger),
+
+            Option('-r', '--reload',
+                   action='store_true',
+                   dest='use_reloader',
+                   help=('monitor Python files for changes (not 100%% safe '
+                         'for production use)'),
+                   default=self.use_reloader),
+            Option('-R', '--no-reload',
+                   action='store_false',
+                   dest='use_reloader',
+                   help='do not monitor Python files for changes',
+                   default=self.use_reloader),
+        )
+        return options
+
+    def __call__(self, app, host, port, use_debugger, use_reloader):
+        # override the default runserver command to start a Socket.IO server
+        if use_debugger is None:
+            use_debugger = app.debug
+            if use_debugger is None:
+                use_debugger = True
+        if use_reloader is None:
+            use_reloader = app.debug
+        socket.run(app,
+                     host=host,
+                     port=port,
+                     debug=use_debugger,
+                     use_reloader=use_reloader,
+                     **self.server_options)
+
+manager.add_command("runserver", Server())
 manager.add_command('shell', Shell(make_context=make_shell_context))
-manager.add_command('runserver', Server('0.0.0.0'))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
