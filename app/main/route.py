118dae56b8dbbf3e999b00bf2f607a0047165459
marvinkome <marvinkome@gmail.com>
2018-06-16 01:41:12 +0100

    Re-add graphql route

--- app/main/route.py
+++ app/main/route.py
@@ -14,6 +14,20 @@ from .. import db, jwt
 from ..model import User
 from ..schema import schema
 
+
+def graphql():
+    graphqli = os.environ.get('USE_GRAPHQLI') or False
+    g = GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        context={'session': db.session},
+        graphiql= graphqli
+    )
+
+    return jwt_optional(g)
+
+main.add_url_rule('/graphql', view_func=graphql())
+
 def validate_password(password):
     regExp = re.compile('^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|\
                         ((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})')
